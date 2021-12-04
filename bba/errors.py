62e00e850ab52e46b30ebcaeabd8e3acb38a86e8
TheGenocide <>
2021-12-04 11:16:34 +0000

    feat: Add new error

--- bba/errors.py
+++ bba/errors.py
@@ -8,3 +8,8 @@ class TooManyRequests(Exception):
     """Exception: Represent error class for response status code 429. This mean you made too much requests."""
     def __init__(self):
         super().__init__("Request return an Exception (code: 429): Ratelimit exceeded! You made too much requests, please wait couple of minutes!")
+
+class InternalServerError(Exception):
+    """Exception: Represent error class for response status code 500. This can mean many things, one of them is invalid image passed in image manipulation endpoint."""
+    def __init__(self):
+        super().__init__("Request return an Exception (code: 500): Invalid image passed.")
